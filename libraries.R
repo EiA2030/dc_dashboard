628b2a1ce3d2da6c2aaed56ee332ba0edeb68be3
Odysseas Stefas <68035017+Odysseas640@users.noreply.github.com>
2022-11-02 15:33:39 +0200

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -4,7 +4,7 @@ shinytest::installDependencies()
 install.packages(pkgs=c('shinydashboard', 'tidyr', 'ggplot2', 'sf', 'lubridate', 'stringr', 'plotly',
 	'shinyBS', 'shinyjs', 'leaflet', 'shinyalert', 'magrittr', 'shinycssloaders', 'reactable',
 	'tippy', 'shinyWidgets', 'auth0', 'data.table', 'dplyr', 'sp', 'future', 'future.apply', 'foreach',
-	'doParallel', 'wrapr', 'devtools'), dependencies=TRUE, repos='https://cran.rstudio.com/')
+	'doParallel', 'wrapr'), dependencies=TRUE, repos='https://cran.rstudio.com/')
 
 
 install.packages(pkgs=c('devtools', 'remotes'), dependencies=TRUE, repos='https://cran.rstudio.com/')
