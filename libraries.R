ec9ce26879f31e96574099fc5ecab3814b86554b
Odysseas Stefas <68035017+Odysseas640@users.noreply.github.com>
2022-11-02 13:10:11 +0200

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -1,11 +1,9 @@
-install.packages('shiny', dependencies=TRUE, repos='https://cran.rstudio.com/')
-install.packages('shinyauthr', dependencies=TRUE, repos='https://cran.rstudio.com/')
+install.packages(pkgs=c('shiny', 'shinyauthr'), dependencies=TRUE, repos='https://cran.rstudio.com/')
 shinytest::installDependencies()
 
 install.packages(pkgs=c('shinydashboard', 'tidyr', 'ggplot2', 'sf', 'lubridate', 'stringr', 'plotly',
 	'shinyBS', 'shinyjs', 'leaflet', 'shinyalert', 'magrittr', 'shinycssloaders', 'reactable',
 	'tippy', 'shinyWidgets', 'auth0', 'data.table', 'dplyr', 'sp', 'future', 'future.apply', 'foreach',
-	'doParallel', 'wrapr'), dependencies=TRUE, repos='https://cran.rstudio.com/')
+	'doParallel', 'wrapr', 'devtools'), dependencies=TRUE, repos='https://cran.rstudio.com/')
 
-install.packages('devtools', dependencies=TRUE, repos='https://cran.rstudio.com/')
 devtools::install_github('onaio/ona.R')
