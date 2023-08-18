2428b6c2faa6c6c22c09a0aefaa77bfce17c653b
Regina Kavive <45850980+reginakavive@users.noreply.github.com>
2023-08-18 16:43:25 +0300

    updates validation version

---
+++ assemble ONA data_validation_NN.R
@@ -0,0 +1,128 @@
+
+#setwd("D:/IITA")
+#wd<-getwd()
+source('okapi.R')
+# Load required libraries
+library(httr)
+library(jsonlite)
+library(tidyr)
+library(purrr)
+library(dplyr)
+library(readr)
+library(stringr)
+
+# Define the ONA server URL, username, password, and form ID
+url <- "https://api.ona.io/api/v1"
+
+# creds <- scan(paste0("./pwd.txt"), what = "character")
+# username <- creds[1]
+# passwo   <- creds[2]
+
+form_id <- "752967" #cassava
+
+#752966    register en
+#752965    register hh
+
+
+dff<-ona_data_get(base_url = "https://api.ona.io", auth_mode =  "token",form_id = 752967)
+
+# # Create the authentication token
+# token <- paste0(username, ":", passwo)
+# auth_token <- base64enc::base64encode(charToRaw(token))
+# 
+# # Make the API request to retrieve data
+# response <- httr::GET(
+#   url = paste0(url, "/data/", form_id),
+#   httr::add_headers(Authorization = paste0("Basic ", auth_token)),
+#   query = list(format = "json")
+# )
+
+data<-ona_data_get(base_url = "https://api.ona.io", auth_mode =  "token",form_id = 752967)
+
+# Parse the JSON response into a data frame with the nesting in list
+# data <- jsonlite::fromJSON(httr::content(response, "text"), flatten = TRUE)
+
+#remove ystem variables
+system_var<- c("_tags","_uuid","_notes" ,"_edited","_status" ,"_version","_duration" ,"_xform_id",
+              "_attachments","_geolocation","_media_count","_total_media","formhub/uuid",
+               "_id","_media_count","_total_media","_submitted_by","_date_modified",
+              "meta/instanceID","_submission_time","intro/geopoint_household",
+               "_xform_id_string","_bamboo_dataset_id","intro/in_the_field","_media_all_received")
+
+data<- data %>% 
+  select(-any_of(system_var))
+
+dataAll_RW<-data
+dataAll_RW<-dataAll_RW %>% 
+  select(-c("start"))
+#------------------------------------------------------------------------------------------
+  #data cleaning
+
+# plant stand data
+Plant_stand_data<- data %>% 
+  dplyr::select(start,today,`intro/country` ,`intro/event`,`intro/latitude`,`intro/longitude`,`intro/altitude`,`intro/enumerator_ID`,`intro/barcodehousehold`,crop,grep("planting.*", names(data), value = TRUE))
+
+#Plot data
+plot_data<- data %>% 
+  dplyr::select(start,today,`intro/country` ,`intro/event`,`intro/latitude`,`intro/longitude`,`intro/altitude`,crop,`intro/enumerator_ID`,`intro/barcodehousehold`,crop,grep("plotDescription.*", names(data), value = TRUE))
+
+plot1<- plot_data %>% 
+  gather(v, value, 12:33) %>% 
+  mutate(treat=ifelse(v %in% grep("*.AEZ.*",v, value=T),"AEZ",
+                      ifelse(v %in% grep("*.BR.*",v, value=T),"BR",
+                             ifelse(v %in% grep("*.SSR.*",v, value=T),"SSR", NA)))) %>% 
+  separate(v, c("details","var", "col"),"/") %>% 
+  select(-details) %>% 
+  mutate(col1=gsub("\\_aez|\\_BR|\\_ssr|\\_control", "", col)) %>% 
+  mutate(col1=gsub("_SSR","",col1)) %>% select(-c(col,var))
+
+# clean col to reshape wide
+
+reshaped_data <- plot1 %>% 
+  pivot_wider(
+  id_cols = c( "start","today","intro/country","intro/event","intro/latitude","intro/longitude","intro/altitude","intro/enumerator_ID", "intro/barcodehousehold", "crop", "plotDescription/plotSizeDetails/row_number","treat"),
+  names_from = col1,
+  values_from = value
+)
+
+ #drop rows that are entirely missing
+
+reshaped_data <- reshaped_data[rowSums(is.na(reshaped_data)) <= ncol(reshaped_data)-5-1, ]
+
+
+# land preparation data
+land_prep_data<- data %>% 
+  dplyr::select(start,today,`intro/country`,`intro/event` ,`intro/latitude`,`intro/longitude`,`intro/altitude`,`intro/enumerator_ID`,`intro/barcodehousehold`,crop,grep("LandPreparation*", names(data), value = TRUE))
+
+
+# crop management data
+
+crop_mgt_data<- data %>% 
+  dplyr::select(start,today,`intro/country`,`intro/event` ,`intro/latitude`,`intro/longitude`,`intro/altitude`,`intro/enumerator_ID`,`intro/barcodehousehold`,crop,grep("cropManagement*", names(data), value = TRUE))
+
+# merge all the datasets
+
+df_list<- list(reshaped_data,Plant_stand_data,land_prep_data,crop_mgt_data) 
+
+full_data<-df_list %>% reduce(full_join, by=c("start","today","intro/country","intro/event","intro/latitude","intro/longitude","intro/altitude","intro/enumerator_ID", "intro/barcodehousehold","crop")) %>% 
+  rename_with(
+    ~stringr::str_replace_all(.x, c("plot_plot/"), ""))
+datacrop_rwa<-full_data
+
+
+#########################################
+ #View(data)
+# unique_subset <- datacrop_rwa %>%
+#   distinct(start,today,`intro/enumerator_ID`,`intro/barcodehousehold`, .keep_all = TRUE)
+datacrop_rwa <- datacrop_rwa%>%
+  rename(
+    ENID = `intro/enumerator_ID`,
+    HHID = `intro/barcodehousehold`
+  )
+
+
+datacropSUB <- datacrop_rwa %>%
+  distinct(start,today,ENID,HHID, .keep_all = TRUE)
+
+datacropSUB$ENID
+
--- support_fun.R
+++ support_fun.R
@@ -1,12 +1,21 @@
+
+usersdata<-read.csv('./data/usecases_updated.csv')
+userList<-as.data.frame(strsplit((paste0(usersdata$users, collapse=",")), ','))[,1]
+passList<-as.data.frame(strsplit((paste0(usersdata$password, collapse=",")), ','))[,1]
+permissionList<-as.data.frame(strsplit((paste0(usersdata$permissions, collapse=",")), ','))[,1]
 #toremove ...replace with auth0 after deploy
 user_base <- dplyr::tibble(
-  user = c("user1", "user2", "user3", "user4"),
-  password = sapply(c("pass1", "pass2", "pass3", "pass4"), sodium::password_store),
-  permissions = c("admin", "standard","admin", "standard"),
-  name = c("User RW1", "User RW2","User NG1", "User NG2")
+  #user = c("user1", "user2", "user3", "user4"),
+  user = unique(userList),
+  #password = sapply(c("pass1", "pass2", "pass3", "pass4"), sodium::password_store),
+  password = sapply(unique(passList), sodium::password_store),
+  #permissions = c("admin", "standard","admin", "standard"),
+  permissions = unique(permissionList)#,
+  #name = c("User RW1", "User RW2","User NG1", "User NG2")
 )
 list1<-c("user1","user2")
 list2<-c("user3","user4")
+ 
 
 #basemap for leaflet
 basemap <- leaflet() %>%
@@ -103,6 +112,124 @@ dropdownMenuCustom <-     function (..., type = c("messages", "notifications", "
 
 
 
+
+
+
+jscode <- "
+shinyjs.hrefAuto = function(url) { window.location.href = url;};
+"
+# Function to create a tab panel
+usecases.index<-c(  " SAA-Nigeria"  =1           , " DigGreen-Ethiopia"  =2  ," Fert-Ethiopia"     =3    , " ex-iSDA-Rwanda"  =4  ,  " ex-iSDA-Ghana"    =5      , " ex-Wcover-Ghana" =6     ,
+                    " Planting-S-Asia" =7     ," DSRC-SE-Asia" =8       ,   " Govt-Egypt"     =9    ," Govt-LatAm"  =10     ,   " Cocoa Soils"  =11   ,   " Rainforest Alliance" =12  ,
+                    " One Acre Fund"    =13     ,  " DRC Coffee OLAM"  =14       ,   " Solidaridad Soy Advisory" =15 , " DSR Extension Vietnam" =16  , " Morocco CA" =17          ,  " Mercy Corps SPROUT"  =18,
+                    " BAYGAP (BAYER)" =19  )
+
+
+
+create_tab_panel <- function(tab_name) {
+
+  uc<-usecases.index[[tab_name]]
+ 
+  tabPanel(tab_name,
+           dashboardPage(
+             dashboardHeader(),
+             dashboardSidebar(
+               width = 200,
+               useShinyjs(),
+               id = paste0("sidebar-", uc), 
+               tags$head(tags$style(HTML("
+                 .collapsible-content {
+                   display: none;
+                   padding: 5px;
+                 }
+                 .collapsed .collapsible-content {
+                   display: none;
+                 }
+                 .expanded .collapsible-content {
+                   display: block;
+                 }
+               "))),
+               sidebarMenu(id = paste0("tabs-", uc), 
+                           menuItem( "DATA", tabName = 'dashboard', icon = icon('dashboard'),startExpanded = T   ,
+
+                                     #filter options
+                                     uiOutput(paste0('stagefinderr_',uc)),
+                                     uiOutput(paste0('experimentfinderr_',uc)), #experiment code
+                                     uiOutput(paste0('datefinderr_',uc)),
+                                     uiOutput(paste0('enumeratorfinderr_',uc)), #enumerators
+                                     uiOutput(paste0('householdfinderr_',uc))  #households
+                           )
+                           ## 'Glossary' tab
+                           #menuItem( "GLOSSARY", tabName = 'glossary', icon = icon('bell') )
+               )
+
+
+             ),
+             dashboardBody(
+               tabsetPanel(
+                 id = paste0("tabss-", uc),  # Unique id for each tabPanel
+                 type = "tabs",
+                 tabPanel(tabName="Summary"  ,"SUMMARY",
+                          HTML('<h3>  Welcome! </h3>'),
+                          #textOutput("selected_var"),
+                          HTML('<h5>  This data monitoring dashboards helps you track enumerator submissions for various trials and check whether various quality checks are adhered to.</h5>'),
+                          HTML('<br>'),
+                          fluidRow( column( width = 12,  align = 'left', infoBoxOutput(  paste0("project_",uc) ) , infoBoxOutput(paste0("country_",uc) ),infoBoxOutput(paste0("Totsub_box_",uc) ))
+                                    #column( width = 6,  align = 'right', uiOutput("Totsub_box"))trials_map submission_trend
+                          ),
+                          fluidRow( column( width = 6,h4("Trials by Location", align = 'center'), leafletOutput(paste0('trials_map_',uc),height = "50vh"),style = "background-color: #f2f2f2;"),
+                                    column( width = 6,h4("Trend of Submissions", align = 'center'), plotlyOutput(paste0('submission_trend_',uc), height = "50vh"),style = "background-color: #f2f2f2;")
+                          ),
+                          HTML('<br>'),
+                          fluidRow( column( width = 12,h4("Summary of Complete Submissions", align = 'center'), reactableOutput(paste0("ranking_",uc)) )
+                          ),
+                 ),
+
+                 tabPanel(tabName="Enumerators","ENUMERATORS",
+                          HTML('<br>'),
+                          HTML('<span  style="background-color: #BFffa590 ;align:right;" class="dot">Complete</span>&nbsp; <span  style="background-color: orange ;align:right;" class="dot">Missing Details</span>&nbsp;<span  style="background-color: #ffa590;align:right;" class="dot">Overdue</span>  &nbsp;<span  style="background-color: #BE93D4;align:left;" class="dot">Future Event</span>'),
+                          HTML('<br>'),
+                          reactableOutput(paste0("tableR_",uc))
+                 ),
+
+                 # tabPanel(tabName="quality" ,"QUALITY CHECKS"
+                 #          #reactableOutput("tableQ1")
+                 # ),
+
+                 tabPanel(tabName= "data","DATA PREVIEW",
+                          column(12,
+                                 dataTableOutput(paste0('tabledownload_',uc)),style = "height:75vh; overflow-y: scroll;overflow-x: scroll;"
+                          ),
+
+                          HTML('<br>'),
+                          downloadButton(paste0("downloadData_",uc), "Download csv",style="color: green")
+                 ),
+                 tabPanel(tabName="glossary" ,"GLOSSARY",
+                          includeHTML('glossary.html')
+                 ),
+
+               )
+
+             )
+           )
+  )
+  
+  
+}
+
+# Function to create navbarMenu with tabPanel elements
+create_navbarMenu <- function(tab_names) {
+  # Create a list of tab panels using lapply
+  tab_panels <- lapply(tab_names, create_tab_panel)
+  
+  # Use do.call to create the navbarMenu with the list of tab panels
+  do.call(navbarMenu, c("Usecase", tab_panels))
+}
+
+
+
+
+
 blank2na = function(x,na.strings=c('','.','NA','na','N/A','n/a','<NA>','NaN','nan')) {
   if (is.factor(x)) {
     lab = attr(x, 'label', exact = T)
@@ -156,10 +283,3 @@ blank2na = function(x,na.strings=c('','.','NA','na','N/A','n/a','<NA>','NaN','na
   }
   return(x)
 }
-
-
-
-
-
-
-
