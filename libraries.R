a330fcaa5529dbd82940a528bdda8ccefdc10ec6
Odysseas Stefas <odysseas@scio.systems>
2023-09-05 17:23:41 +0300

    Add tinytex, rmarkdown

--- libraries.R
+++ libraries.R
@@ -10,3 +10,7 @@ shinytest::installDependencies()
 devtools::install_github('onaio/ona.R')
 remotes::install_github('rapidsurveys/odktools')
 install.packages("cronR", repos = "http://cran.us.r-project.org")
+
+install.packages('shinythemes', repos='https://cran.rstudio.com/')
+install.packages('rmarkdown', repos='https://cran.rstudio.com/')
+tinytex::install_tinytex()
