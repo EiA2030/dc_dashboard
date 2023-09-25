b050e928c61e74f932676d53ddd397ac08ebc01f
Regina Kavive <45850980+reginakavive@users.noreply.github.com>
2023-09-25 10:59:54 +0300

    updates check

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 
+setwd("/root/dc_dashboard/")
 # load packages
 suppressMessages(suppressWarnings(library("shiny",character.only = TRUE)))
 suppressMessages(suppressWarnings(library("shinyauthr",character.only = TRUE)))
@@ -33,7 +34,7 @@ suppressMessages(suppressWarnings(library("rmarkdown",character.only = TRUE)))
 #suppressMessages(suppressWarnings(library("memoise",character.only = TRUE)))
 
 #tinytex::install_tinytex()
-#source('dataprocessing.R')
+source('dataprocessing.R')
 
 ## load functions+files
 source('support_fun.R')
