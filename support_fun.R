83940795a3b88c2c23f9d2b83333b360038bec05
Regina Kavive <45850980+reginakavive@users.noreply.github.com>
2023-09-28 10:05:34 +0300

    to check data updates: run everything in-app

--- support_fun.R
+++ support_fun.R
@@ -1,8 +1,8 @@
 #load data(updated daily)
 
 #SNS.Rwanda.VAL_data<-read.csv("./data/Usecases/SNS-Rwanda/SNS-Rwanda.VAL_data.csv") 
-SNS.Rwanda.SUM_data<-read.csv("./data/SNSRwandaSUMdata.csv") 
-SNS.Rwanda.O_data<-read.csv("./data/SNSRwandaOdata.csv") 
+# SNS.Rwanda.SUM_data<-read.csv("./data/SNSRwandaSUMdata.csv") 
+# SNS.Rwanda.O_data<-read.csv("./data/SNSRwandaOdata.csv") 
 usersdata<-read.csv('./data/usecases_updated.csv')
 userList<-as.data.frame(strsplit((paste0(usersdata$users, collapse=",")), ','))[,1]
 passList<-as.data.frame(strsplit((paste0(usersdata$password, collapse=",")), ','))[,1]
